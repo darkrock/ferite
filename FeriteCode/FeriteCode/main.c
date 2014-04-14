20da6db08b5483fcb5525d068c9ded6e1caca41e
Chris Ross <chris@darkrock.co.uk>
2014-04-14 15:24:41 +0100

    Project

---
+++ FeriteCode/FeriteCode/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  FeriteCode
+//
+//  Created by Chris Ross on 14/04/2014.
+//  Copyright (c) 2014 hiddenMemory Limited. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
